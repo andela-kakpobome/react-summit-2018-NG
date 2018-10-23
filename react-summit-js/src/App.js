722fa76d7feb13ff9ab465a8d5fadeb76008b5bf
Kesiena Akpobome <bourgeoisie247@gmail.com>
2018-10-23 13:04:45 +0100

    fix(JS): pass all the required props
    - used the destructuring technique as a shorthand
    - you can decide to pass each individual prop

--- react-summit-js/src/App.js
+++ react-summit-js/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <Profile name={this.state.name}/>
+        <Profile { ...this.state } />
       </div>
     );
   }
